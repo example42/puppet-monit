5a3b8a097667f74c1b789df4c224be85f1e28602
CV <dagobert@users.noreply.github.com>
2018-12-11 14:52:23 +0100

    Missed removing one line "audit..."
    In an earlier fix the option "$autit" was removed. You missed one line.

--- manifests/init.pp
+++ manifests/init.pp
@@ -478,7 +478,6 @@ class monit (
     source  => $monit::manage_file_source,
     content => $monit::manage_file_content,
     replace => $monit::manage_file_replace,
-    audit   => $monit::manage_audit,
   }
 
   file { 'monit.init':
